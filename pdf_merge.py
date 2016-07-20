737b7cd9e903c24243120aa74f67293cb739c15e
nagracks <nagracks@users.noreply.github.com>
2016-07-20 09:13:59 +0530

    Refactored

--- pdf_merge.py
+++ pdf_merge.py
@@ -8,22 +8,21 @@ __copyright__ = "Copyright © 2016 nagracks"
 
 import argparse
 
-# external modules #
+# external modules
 import PyPDF2
 
 def parse_args():
     """Parse commandline args with argparse
     :returns: args
-
     """
     parser = argparse.ArgumentParser(description="PDF Merge script")
-    # Positional args #
+    # Positional args
     parser.add_argument('files',
                         action='store',
                         nargs='*',
                         help="pdf files")
 
-    # Optional args #
+    # Optional args
     parser.add_argument('-o',
                         '--output-file',
                         dest='output_file',
@@ -35,7 +34,11 @@ def parse_args():
 
 class PDFMerge(object):
 
-    """PDF Merge"""
+    """PDF Merge
+    
+    methods:
+    * merge_it
+    """
 
     def __init__(self, files):
         """Initialise class
@@ -47,34 +50,28 @@ class PDFMerge(object):
         """Merge PDF files
         :returns: None
         """
-        # Initialise merger class #
+        # Initialise merger class
         merger = PyPDF2.PdfFileMerger()
 
-        # Iterate through input pdf files #
-        # Append pdfs one after another #
+        # Iterate through input pdf files
+        # Append pdfs one after another
         for filename in self.files:
             merger.append(open(filename, 'rb'))
 
-        # Finally write to file #
+        # Finally write to file
         merger.write(output_name)
 
-def main():
-    """Main function
-    :returns: TODO
-    """
-    # Commandline args #
+if __name__ == "__main__":
+    # Commandline args
     args = parse_args()
 
-    # Initialise class #
+    # Merger object
     merge = PDFMerge(args.files)
 
-    # Conditions when merging PDFs #
-    # if output filename is given, use it as output filename #
+    # Conditions when merging PDFs
+    # If output filename is given, use it as output filename
     if args.output_file:
         merge.merge_it(args.output_file)
-    # Else merge it with default name `merged_file.pdf` #
+    # Else merge it with default name `merged_file.pdf`
     else:
         merge.merge_it()
-
-if __name__ == "__main__":
-    main()
