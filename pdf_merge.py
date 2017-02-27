7c1e05ca28a7a8dd8b9a744b96e9ea414338c17e
nagracks <nagracks@users.noreply.github.com>
2017-02-27 18:57:14 +0530

    Refactor

--- pdf_merge.py
+++ pdf_merge.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 __author__    = "nagracks"
@@ -8,70 +8,42 @@ __copyright__ = "Copyright © 2016 nagracks"
 
 import argparse
 
-# external modules
 import PyPDF2
 
-def parse_args():
-    """Parse commandline args with argparse
-    :returns: args
+
+def merge_it(files, output_name='merged_file.pdf'):
+    """Merge PDF files
+
+    :files: list of pdf files
+    :output_name: name of output merged file
+    :returns: None
     """
-    parser = argparse.ArgumentParser(description="PDF Merge script")
-    # Positional args
-    parser.add_argument('files',
-                        action='store',
-                        nargs='*',
-                        help="pdf files")
+    if len(files) <= 1:
+        return
+    merger = PyPDF2.PdfFileMerger()
+    for filename in files:
+        merger.append(open(filename, 'rb'))
+    merger.write(output_name)
 
-    # Optional args
-    parser.add_argument('-o',
-                        '--output-file',
-                        dest='output_file',
-                        action='store',
-                        help="output file name. defaults to `merged_file.pdf`")
-
-    args = parser.parse_args()
-    return args
-
-class PDFMerge(object):
-
-    """PDF Merge
-    
-    methods:
-    * merge_it
-    """
-
-    def __init__(self, files):
-        """Initialise class
-        :files: input files
-        """
-        self.files = files
-
-    def merge_it(self, output_name='merged_file.pdf'):
-        """Merge PDF files
-        :returns: None
-        """
-        # Initialise merger class
-        merger = PyPDF2.PdfFileMerger()
-
-        # Iterate through input pdf files
-        # Append pdfs one after another
-        for filename in self.files:
-            merger.append(open(filename, 'rb'))
-
-        # Finally write to file
-        merger.write(output_name)
 
 if __name__ == "__main__":
-    # Commandline args
-    args = parse_args()
+    # Parse commandline options with argparse
+    parser = argparse.ArgumentParser(description="PDF Merge script")
+    parser.add_argument(
+            'files', 
+            action='store', 
+            nargs='*',
+            help="pdf files"
+            )
+    parser.add_argument(
+            '-o', '--output-file',
+            dest='output_file',
+            action='store',
+            help="output file name. defaults to `merged_file.pdf`"
+            )
+    args = parser.parse_args()
 
-    # Merger object
-    merge = PDFMerge(args.files)
-
-    # Conditions when merging PDFs
-    # If output filename is given, use it as output filename
     if args.output_file:
-        merge.merge_it(args.output_file)
-    # Else merge it with default name `merged_file.pdf`
+        merge_it(args.files, args.output_file)
     else:
-        merge.merge_it()
+        merge_it(args.files)
